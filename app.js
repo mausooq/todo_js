320db22621373839beec16ee94b5590299a9cc6b
mausooq <mauchoomausooq051@gmail.com>
2024-01-29 22:05:59 +0530

    new\

--- app.js
+++ app.js
@@ -6,13 +6,34 @@ app.use(bodyParser.json());
 
 let todos = [];
 
+function findIndex(arr,index){
+    for(i=0;i<arr.length;i++){
+        if(arr[i].id === index){
+            return i;
+        }
+        return -1;
+    }
+}
 app.get('/todos',(req,res) => {
-    res.json(todos)
+   res.json(todos);
 })
-app.get('/todos/:id')
-app.post('/todos/:id')
-app.put('/todos')
-app.delete('/todos/:id')
+app.get('/todos/:id',(req,res) => {
+   const todoIndex = findIndex(todos , parseInt(req.params.id));
+    if(todoIndex == -1)
+        res.status(404).send("NOT FOUND");
+    else{
+        res.json(todos[todoIndex]);
+    }
+})
+app.post('/todos/:id',(req,res) => {
+    const {itle,description}=req.body;
+})
+app.put('/todos',(req,res) => {
+    console.log(`todo update`)
+})
+app.delete('/todos/:id',(req,res) => {
+    console.log(`todo delete id: ${req.params.id}`)
+})
 
 app.listen(2000,(req,res) => {
     console.log(`server started on port  2000`)
