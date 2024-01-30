1809475ee1da5d142881975fcf12b85e0dd63b1a
mausooq <mauchoomausooq051@gmail.com>
2024-01-30 22:18:42 +0530

    new

--- app2.js
+++ app2.js
@@ -5,11 +5,50 @@ const fs = require('fs')
 const app =express();
 app.use(bodyParser.json());
 
-app.get('todos',(req,res) => {
+function findIndex(arr,id){
+    for (let i=0;i<arr.length;i++){
+        if(arr[i].id === id)
+             return i;
+        else return -1;
+    }
+
+}
+app.get('/todos',(req,res) => {
     fs.readFile("todos.json","utf-8",(err,data) => {
         if(err) throw err;
-        res.json(JSON.parse(date))
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post('/todos',(req,res) => {
+        const newTodo={
+            id:Math.floor(Math.random() * 10000000),
+            title:req.body.title,
+            description:req.body.description
+        };
+      fs.readFile("todos.json","utf-8",(err,data)=>{
+        if(err) throw err
+        const todos =JSON.parse(data)
+        todos.push(newTodo)
+        fs.writeFile('todos.json', JSON.stringify(todos), 'utf-8', (err) => {
+            if (err) throw err
+            res.status(201).json(newTodo); 
+        });
+      })
+ 
+})
+app.get('/todos/:id',(req,res)=> {
+    fs.readFile("todos.json","utf-8",(err,data)=> {
+        todoData = JSON.parse(data);
+        const todoIndex = findIndex(todoData,parseInt(req.params.id));
+        if(todoIndex===-1){
+            res.status(404)
+        }
+        else{
+            res.json(todoData[todoIndex]);
+        }
     })
+   
 })
 app.listen(2000,()=> {
     console.log("app2.js  Server is running on port 2000");
